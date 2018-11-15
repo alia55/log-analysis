60079956373d89457b64a3f23d7d1577b698c1bc
alia55 <alia55.m@hotmail.com>
2018-11-15 15:32:08 +0300

    Update log-analysis.py

--- log-analysis.py
+++ log-analysis.py
@@ -1,38 +1,50 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: latin-1 -*-
-import os, sys
+import os
+import sys
 import psycopg2
 
-#from datetime import datetime
 DBNAME = "news"
 
 
-
 def get_articles():
-  """Return most popular three articles of all time"""
+    """Return most popular three articles of all time"""
+
+
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-c.execute("select count(path) as views,title from articles,log where  concat('/article/',slug) =log.path group by title order by views desc limit 3")
+c.execute("select count(path) as views,title from articles,log\
+ where  concat('/article/',slug) =log.path\
+ group by title order by views desc limit 3")
 articles = c.fetchall()
 db.close()
 for row in articles:
     print "\"%s\" — %s views" % (row[1], row[0])
 
+
 def get_authors():
-  """Return most popular three articles of all time"""
+    """Return most popular three articles of all time"""
+
+
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-c.execute("select count(path) as views,name from articles,log,authors where  concat('/article/',slug) =log.path and articles.author = authors.id group by authors.id order by views desc")
+c.execute("select count(path) as views,name from articles,log,authors\
+ where  concat('/article/',slug) =log.path\
+ and articles.author = authors.id group by authors.id order by views desc")
 articles = c.fetchall()
 db.close()
 for row in articles:
     print "%s — %s views" % (row[1], row[0])
 
+
 def get_errors():
-  """Return most popular three articles of all time"""
+    """Return most popular three articles of all time"""
+
+
 db = psycopg2.connect(database=DBNAME)
 c = db.cursor()
-c.execute("Select to_char(date,'Mon dd,yyyy'), error *100.0 /total as percent from summary where error *100.0 /total >1 ")
+c.execute("Select to_char(date,'Mon dd,yyyy'), error *100.0 /total as percent\
+ from summary where error *100.0 /total >1 ")
 articles = c.fetchall()
 db.close()
 for row in articles:
